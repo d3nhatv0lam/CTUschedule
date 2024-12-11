41b0777fcea70d5275ae0e4f21886a07ba171f7d
Đức Dương <d3nhatv0lam@gmail.com>
2024-12-11 20:19:51 +0700

    Add project files.

---
+++ CTUschedule/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CTUschedule.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        [ObservableProperty]
+        public List<ViewModelBase> _pageViewModels = new List<ViewModelBase>();
+        [ObservableProperty]
+        public ViewModelBase _currentViewModel;
+       
+
+
+        public MainWindowViewModel()
+        {
+            PageViewModels.Add(new SignInViewModel());
+
+            CurrentViewModel = PageViewModels.First();
+        }
+    }
+}
---
+++ CTUschedule/Views/SignInView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CTUschedule.Views;
+
+public partial class SignInView : UserControl
+{
+    public SignInView()
+    {
+        InitializeComponent();
+    }
+}
